fb83fe8d82c72a72b805560181c8d27ceed4f448
Tianyi Shi <ShiTianyi2001@outlook.com>
2020-09-10 12:44:36 +0800

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// Copyright (C) 2020 Tianyi Shi
+//
+// This file is part of rust-bio-edu.
+//
+// rust-bio-edu is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// rust-bio-edu is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with rust-bio-edu.  If not, see <http://www.gnu.org/licenses/>.
